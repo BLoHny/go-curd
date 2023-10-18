4235420a59a6b751e1b0df21ebd33b4fba55458b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-10-18 13:07:01 +0900

    DB 설정

---
+++ config/database.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/blohny/error"
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbName   = "test"
+)
+
+func databaseConnection() *sql.DB {
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+
+	db, err := sql.Open("postgres", sqlInfo)
+	error.PanicIfError(err)
+
+	err = db.Ping()
+	error.PanicIfError(err)
+
+	log.Info().Msg("Connected to database!!")
+
+	return db
+}
